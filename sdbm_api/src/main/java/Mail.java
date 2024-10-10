02341fc3e1eb398def1aa5e504e4edd98a307419
sayana <sayana.ear@gmail.com>
2024-10-10 13:01:01 +0200

    class mail SDBM

---
+++ sdbm_api/src/main/java/Mail.java
@@ -0,0 +1,51 @@
+public class Mail {
+    private int id;
+    private String sendAt;
+    private String sendTo;
+    private String subject;
+    private String apiKey;
+
+    public Mail(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSendAt() {
+        return sendAt;
+    }
+
+    public void setSendAt(String sendAt) {
+        this.sendAt = sendAt;
+    }
+
+    public String getSendTo() {
+        return sendTo;
+    }
+
+    public void setSendTo(String sendTo) {
+        this.sendTo = sendTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+}
