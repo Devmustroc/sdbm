7e8c7134d044c24d37b251655a2e1f7e4f5bb0d2
DanielMM2 <128694954+DanielMM2@users.noreply.github.com>
2024-10-11 08:46:56 +0200

    Add files via upload

---
+++ Marque.java
@@ -0,0 +1,38 @@
+package sdbm_api;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Marque extends PanacheEntityBase {
+
+      @Id
+	@GeneratedValue (strategy GenerationType.IDENTITY)
+ 	@Column(id="ID_MARQUE");
+	private Long id;
+      @Column (name="NOM_MARQUE");
+      private String name;
+    @OneToMany()
+    	private List<Article> articles = new ArrayList<>();
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
