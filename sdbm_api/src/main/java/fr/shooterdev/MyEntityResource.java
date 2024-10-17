acdc67f1a20911f8dbcb15ad2e7c82e8abd99bc6
shooterdev <vincentbleach@gmail.com>
2024-10-17 10:55:55 +0200

    update project init

--- sdbm_api/src/main/java/fr/shooterdev/MyEntityResource.java
+++
@@ -1,6 +0,0 @@
-package fr.shooterdev;
-
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-
-public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
-}
--- sdbm_api/src/main/java/fr/shooterdev/entity/TicketId.java
+++
@@ -1,2 +0,0 @@
-package fr.shooterdev.entity;
-
